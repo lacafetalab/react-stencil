bf1ce2a70bf0859cf168940d2c0700a5c88777fe
Renzo Telenta <renzo@lacafetalab.pe>
2020-01-28 17:13:57 -0500

    first commit

---
+++ babel-plugin-macros.config.js
@@ -0,0 +1,8 @@
+const tailwindconfig = require.resolve("cafeta-components/tailwind.config");
+
+module.exports = {
+  tailwind: {
+    config: tailwindconfig,
+    styled: "styled-components"
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MyComponent } from "cafeta-components/react/dist";
+import styled from "styled-components";
+import tw from "tailwind.macro";
+
+const Prueba = styled(MyComponent)`
+  ${tw`flex items-center justify-center bg-primary mt-5`}
+`;
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Prueba></Prueba>
+      <MyComponent first="asdas" last="asdas" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +30,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
